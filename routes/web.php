8983e7d2a21d2dfaa2a815192f87b2a1333a80e1
jamper007 <jamperone@gmail.com>
2025-04-28 11:25:06 -0500

    Actualizacion del proyecto

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::post('/guardar-foto', [FotoController::class, 'guardar']);
 
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
